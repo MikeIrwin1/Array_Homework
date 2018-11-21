1396ef378b7cd900bab92ee2cbd7e4504859fd24
Michael Irwin <mike.irwin1@gmail.com>
2018-11-21 17:46:05 +0000

    exercise 6 complete

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -75,6 +75,15 @@ p users["Avril"][:pets][0][:species]
 p users["Erik"][:lottery_numbers].min
 
 # 6. Return an array of Avril's lottery numbers that are even
+
+lottery = users["Avril"][:lottery_numbers]
+
+for num in lottery
+  if num % 2 == 0
+    p num
+  end
+end
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "Fluffy"
