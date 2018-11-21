9864960c952fdc99cd6b2ead047be678aeeb6e18
Michael Irwin <mike.irwin1@gmail.com>
2018-11-21 17:11:38 +0000

    all exercises complete

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -2,40 +2,42 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 # 1. Add `"Edinburgh Waverley"` to the end of the array
 
-#stops.push("Edinburgh Waverley")
+stops.push("Edinburgh Waverley")
 
 # 2. Add `"Glasgow Queen St"` to the start of the array
 
-#stops.unshift("Glasgow Queen St")
+stops.unshift("Glasgow Queen St")
 
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
 
-#stops.insert(4, "Polmont")
+stops.insert(4, "Polmont")
 
 # 4. Work out the index position of `"Linlithgow"`
 
-#stops.index("Linlithgow")
+stops.index("Linlithgow")
 
 # 5. Remove `"Livingston"` from the array using its name
 
-#stops.delete("Livingston")
+stops.delete("Livingston")
 
 # 6. Delete `"Cumbernauld"` from the array by index
 
-#stops.delete_at(1)
-
+stops.delete_at(1)
 
 # 7. How many stops there are in the array?
 
-#stops_in_array = stops.count
+stops_in_array = stops.count
 
 # 8. How many ways can we return `"Falkirk High"` from the array?
 
-#return by index or return by element = 2
-
+return by index or return by element = 2
+#
 # 9. Reverse the positions of the stops in the array
 
 stops_reverse = stops.reverse
-p stops_reverse
 
 # 10. Print out all the stops using a for loop
+
+for stop in stops
+  p stop
+end
