11c77721921c5940bca4625edf2a983db5f83116
Michael Irwin <mike.irwin1@gmail.com>
2018-11-21 17:49:06 +0000

    exercise 8 complete

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -89,5 +89,8 @@ end
 p users["Erik"][:lottery_numbers]<< 7
 
 # 8. Change Erik's hometown to Edinburgh
+
+p users["Erik"][:home_town] = "Edinburgh"
+
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10. Add another person to the users hash
